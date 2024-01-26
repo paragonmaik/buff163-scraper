ee247344781d34f4e8ae2fb153d414b3c38a0f3f
maik <estevan.maik@gmail.com>
2024-01-26 17:26:20 -0300

    chore: adds main function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+BASE_URL = "https://buff.163.com/market/csgo"
+
+service = Service()
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service,options=options)
 
 def search():
-    pass
+    print("Hello Sailor")
+
+def main():
+    search()
+
+if __name__ == "__main__":
+    main()
