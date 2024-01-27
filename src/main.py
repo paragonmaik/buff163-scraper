0e27d9b7d317c7c9a453b5be5b89975093a59eb8
maik <estevan.maik@gmail.com>
2024-01-27 16:08:09 -0300

    feat: finished filter_by_percentage func

--- src/main.py
+++ src/main.py
@@ -33,30 +33,40 @@ def get_filtered_percentage_arg() -> float:
 # quantity
 def filter_by_percentage(
     actual_pct: float, filtered_pct: float
-) -> None:
-    actual_pct = filtered_pct
+) -> bool:
+    if actual_pct >= filtered_pct:
+        return True
+    return False
 
 
-def search_skins() -> None:
+def search_skins(diff_pct: float) -> None:
     card_csgo = driver.find_element(By.CLASS_NAME, "card_csgo")
     items_list = card_csgo.find_elements(By.TAG_NAME, "li")
 
     for item in items_list:
         values_list = item.find_elements(By.TAG_NAME, "span")
-        if len(values_list) > 0:
-            print(
-                "Price diff and percentage: ",
-                values_list[-1].text,
-            )
-            print("Ask: ", values_list[-6].text)
-            print("Bid: ", values_list[-4].text)
+        # TODO: add safe guard by negating condition
+        # and return None
+        if len(values_list) < 1:
+            return
+
+        price_diff = values_list[-1].text
+        price_ask = values_list[-6].text
+        price_bid = values_list[-4].text
+        if filter_by_percentage(
+            float(price_diff[-4:-1]), diff_pct
+        ):
+            print("Price diff and percentage: ", price_diff)
+            print("Ask: ", price_ask)
+            print("Bid: ", price_bid)
 
 
 def main():
+    # TODO: rename func and var
     filtered_pct = get_filtered_percentage_arg()
-    print(filtered_pct)
+    # print(filtered_pct)
     driver.get(BASE_URL)
-    search_skins()
+    search_skins(filtered_pct)
     while True:
         pass
 
