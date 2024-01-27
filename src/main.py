634c9e4f385871cead560900e7a4202540c52c51
maik <estevan.maik@gmail.com>
2024-01-26 23:07:51 -0300

    feat: add extension loading to chromedriver instance

--- src/main.py
+++ src/main.py
@@ -3,13 +3,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 BASE_URL = "https://buff.163.com/market/csgo"
+# ABSOLUTE_PATH_UNPACKED = '/home/maik/Dev/BuffUtility'
+ABSOLUTE_PATH_PACKED = '/home/maik/Dev/cs-automaton/src/extensions/BuffUtility.crx'
 
+# Initial Setup
 service = Service()
 options = webdriver.ChromeOptions()
+
+options.add_extension(ABSOLUTE_PATH_PACKED)
+
 driver = webdriver.Chrome(service=service,options=options)
 
 def search():
-    print("Hello Sailor")
+    driver.get(BASE_URL)
+    while(True):
+        pass
 
 def main():
     search()
