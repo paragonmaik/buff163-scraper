637b828223f7ab3c0423205476a70e6ddbc4a1d5
maik <estevan.maik@gmail.com>
2024-01-29 16:11:22 -0300

    feat: sets up func that adds skins to list

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -4,31 +4,50 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
+from typing import List
 
 
 class Skin:
     def __init__(
-        self, name, url, wear, ask, bid, price_diff, pct_diff
+        self,
+        name: str | None,
+        url: str | None,
+        ask_quantity: float,
+        ask_price: float,
+        bid_quantity: float,
+        bid_price: float,
+        price_diff: float,
+        pct_diff: float,
+        wear: str,
     ) -> None:
         self.name = name
         self.url = url
-        self.wear = wear
-        self.ask = ask
-        self.bid = bid
+        self.ask_quantity = ask_quantity
+        self.ask_price = ask_price
+        self.bid_quantity = bid_quantity
+        self.bid_price = bid_price
         self.price_diff = price_diff
         self.pct_diff = pct_diff
+        self.wear = wear
 
-    name: str = ""
-    url: str = ""
-    wear: str = ""
-    ask: str = ""
-    bid: str = ""
+    name: str | None = ""
+    url: str | None = ""
+    ask_quantity: float = 0
+    ask_price: float = 0
+    bid_quantity: float = 0
+    bid_price: float = 0
     price_diff: float = 0
     pct_diff: float = 0
+    wear: str = ""
+
+    def __str__(self) -> str:
+        return ""
 
 
 # variables
+# TODO: add as env variables
 BASE_URL = "https://buff.163.com/market/csgo"
 # ABSOLUTE_PATH_UNPACKED = '/home/maik/Dev/BuffUtility'
 ABSOLUTE_PATH_PACKED = (
@@ -56,6 +75,9 @@ def filter_by_float_value(
     return False
 
 
+# def result_logger(skins_list: List[WebElement]) -> None:
+
+
 def search_skins(
     diff_pct: float,
     diff_price: float,
@@ -70,11 +92,14 @@ def search_skins(
 
     for item in items_list:
         values_list = item.find_elements(By.TAG_NAME, "span")
-        url = item.find_element(By.TAG_NAME, "a")
+        url_element = item.find_element(By.TAG_NAME, "a")
+
         if len(values_list) < 1:
             return
 
-        price_diff = values_list[-1].text
+        price_pct_diff = values_list[-1].text
+        pct_diff = price_pct_diff[-5:-1]
+        price_diff = price_pct_diff.split("|", 1)[0][1:]
         price_ask = values_list[-6].text
         price_bid = values_list[-4].text
         skin_wear = values_list[-7].text
@@ -84,14 +109,17 @@ def search_skins(
         bid_quantity = values_list[-4].text.split("(", 1)[-1][
             :-1
         ]
+        skin_name = url_element.get_attribute("title")
+        skin_url = url_element.get_attribute("href")
 
         if not filter_by_float_value(
-            float(price_diff[-5:-1]), diff_pct
+            float(price_pct_diff[-5:-1]), diff_pct
         ):
             continue
 
         if not filter_by_float_value(
-            float(price_diff.split("|", 1)[0][1:]), diff_price
+            float(price_pct_diff.split("|", 1)[0][1:]),
+            diff_price,
         ):
             continue
 
@@ -106,13 +134,28 @@ def search_skins(
             continue
 
         count += 1
-        print("Price diff and percentage: ", price_diff)
-        print("Ask: ", price_ask)
-        print("Bid: ", price_bid)
-        print("Ask quantity: ", ask_quantity)
-        print("Bid quantity: ", bid_quantity)
+        print("Name: ", skin_name)
         print("Skin wear: ", skin_wear)
-        print("Diff with: ", price_diff.split("|", 1)[0][1:])
-        print("URL: ", url.get_attribute("href"))
-        print("Name: ", url.get_attribute("title"))
+        print(
+            "Price diff and percentage: ",
+            price_diff,
+            "|",
+            pct_diff,
+        )
+        print("Ask price: ", price_ask)
+        print("Ask quantity: ", ask_quantity)
+        print("Bid price: ", price_bid)
+        print("Bid quantity: ", bid_quantity)
+        print("URL: ", skin_url)
+        skin = Skin(
+            skin_name,
+            skin_url,
+            float(price_ask),
+            float(ask_quantity),
+            float(price_bid),
+            float(bid_quantity),
+            float(price_diff),
+            float(pct_diff),
+            skin_wear,
+        )
     print(count)
