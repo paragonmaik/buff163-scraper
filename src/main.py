713b55df70c32c1235c923618464c32814b23fa1
maik <estevan.maik@gmail.com>
2024-01-29 21:21:52 -0300

    chore: temporarly moved logger func to scraper module

--- src/main.py
+++ src/main.py
@@ -1,35 +1,5 @@
-from selenium.webdriver.remote.webelement import WebElement
-from typing import List
 from commands.reader import read_flags, USED_COMMANDS_LIST
 from scraper.scraper import search_skins
-
-# variables
-BASE_URL = "https://buff.163.com/market/csgo"
-# ABSOLUTE_PATH_UNPACKED = '/home/maik/Dev/BuffUtility'
-ABSOLUTE_PATH_PACKED = (
-    "/home/maik/Dev/cs-automaton/src/extensions/BuffUtility.crx"
-)
-
-
-class Skin:
-    def __init__(
-        self, name, url, wear, ask, bid, price_diff, pct_diff
-    ) -> None:
-        self.name = name
-        self.url = url
-        self.wear = wear
-        self.ask = ask
-        self.bid = bid
-        self.price_diff = price_diff
-        self.pct_diff = pct_diff
-
-    name: str = ""
-    url: str = ""
-    wear: str = ""
-    ask: str = ""
-    bid: str = ""
-    price_diff: float = 0
-    pct_diff: float = 0
 
 
 def get_float_type_command(flag: str) -> float:
@@ -38,19 +8,10 @@ def get_float_type_command(flag: str) -> float:
     return 0
 
 
-def result_logger(skins_list: List[WebElement]) -> None:
-    price_diff = skins_list[-1].text
-    price_ask = skins_list[-6].text
-    price_bid = skins_list[-4].text
-
-    print("Price diff and percentage: ", price_diff)
-    print("Ask: ", price_ask)
-    print("Bid: ", price_bid)
-
-
 def main():
     read_flags()
 
+    # TODO: move to commands module
     filtered_pct = get_float_type_command("-p")
     filtered_price_diff = get_float_type_command("-v")
     filtered_ask = get_float_type_command("-q")
@@ -61,7 +22,6 @@ def main():
         filtered_ask,
         filtered_bid,
     )
-    # driver.get(BASE_URL)
 
     search_skins(
         filtered_pct,
