b71ad1a1813510201409ba5f6798a8c10fda293f
maik <estevan.maik@gmail.com>
2024-01-29 21:23:10 -0300

    chore: adds result_logger func

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -4,47 +4,8 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
-from typing import List
-
-
-class Skin:
-    def __init__(
-        self,
-        name: str | None,
-        url: str | None,
-        ask_quantity: float,
-        ask_price: float,
-        bid_quantity: float,
-        bid_price: float,
-        price_diff: float,
-        pct_diff: float,
-        wear: str,
-    ) -> None:
-        self.name = name
-        self.url = url
-        self.ask_quantity = ask_quantity
-        self.ask_price = ask_price
-        self.bid_quantity = bid_quantity
-        self.bid_price = bid_price
-        self.price_diff = price_diff
-        self.pct_diff = pct_diff
-        self.wear = wear
-
-    name: str | None = ""
-    url: str | None = ""
-    ask_quantity: float = 0
-    ask_price: float = 0
-    bid_quantity: float = 0
-    bid_price: float = 0
-    price_diff: float = 0
-    pct_diff: float = 0
-    wear: str = ""
-
-    def __str__(self) -> str:
-        return ""
-
+from skin import Skin, SKINS_LIST
 
 # variables
 # TODO: add as env variables
@@ -53,20 +14,12 @@ BASE_URL = "https://buff.163.com/market/csgo"
 ABSOLUTE_PATH_PACKED = (
     "/home/maik/Dev/cs-automaton/src/extensions/BuffUtility.crx"
 )
-
 service = Service()
 options = webdriver.ChromeOptions()
 options.add_extension(ABSOLUTE_PATH_PACKED)
 driver = webdriver.Chrome(service=service, options=options)
 
 
-# TODO: add func to concatenate skins into a list that
-# can be logged and written into file
-# def add_skins_to_list()
-
-
-# TODO: use the same func for all comparisons with the same
-# signature
 def filter_by_float_value(
     actual: float, filtered: float
 ) -> bool:
@@ -75,7 +28,9 @@ def filter_by_float_value(
     return False
 
 
-# def result_logger(skins_list: List[WebElement]) -> None:
+def result_logger() -> None:
+    for skin in SKINS_LIST:
+        print(skin)
 
 
 def search_skins(
@@ -100,8 +55,8 @@ def search_skins(
         price_pct_diff = values_list[-1].text
         pct_diff = price_pct_diff[-5:-1]
         price_diff = price_pct_diff.split("|", 1)[0][1:]
-        price_ask = values_list[-6].text
-        price_bid = values_list[-4].text
+        price_ask = values_list[-6].text.split("ask", 1)[0][1:]
+        price_bid = values_list[-4].text.split("bid", 1)[0][1:]
         skin_wear = values_list[-7].text
         ask_quantity = values_list[-6].text.split("(", 1)[-1][
             :-1
@@ -134,28 +89,17 @@ def search_skins(
             continue
 
         count += 1
-        print("Name: ", skin_name)
-        print("Skin wear: ", skin_wear)
-        print(
-            "Price diff and percentage: ",
-            price_diff,
-            "|",
-            pct_diff,
-        )
-        print("Ask price: ", price_ask)
-        print("Ask quantity: ", ask_quantity)
-        print("Bid price: ", price_bid)
-        print("Bid quantity: ", bid_quantity)
-        print("URL: ", skin_url)
         skin = Skin(
             skin_name,
             skin_url,
+            int(ask_quantity),
             float(price_ask),
-            float(ask_quantity),
+            int(bid_quantity),
             float(price_bid),
-            float(bid_quantity),
             float(price_diff),
             float(pct_diff),
             skin_wear,
         )
+        SKINS_LIST.append(skin)
+        result_logger()
     print(count)
