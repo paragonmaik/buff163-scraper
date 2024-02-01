b65045b2874c9ac80971bd7266715969c0eb70e7
maik <estevan.maik@gmail.com>
2024-02-01 14:24:28 -0300

    feat: adds option 2 to load settings

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from scraper.scraper import scrape
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from logger.logger import log
+from settings.reader import (
+    handle_settings,
+    read_settings_on_bot_load,
+)
 
 
 BUFF_MARKET_URL = "https://buff.163.com/market/csgo"
@@ -11,16 +15,17 @@ BUFF_BASE_URL = "https://buff.163.com/"
 ABSOLUTE_PATH_PACKED = (
     "/home/maik/Dev/cs-automaton/extensions/BuffUtility.crx"
 )
+ABSOLUTE_SETTINGS_PATH = (
+    "/home/maik/Dev/cs-automaton/settings.json"
+)
 service = Service()
 options = webdriver.ChromeOptions()
 options.add_extension(ABSOLUTE_PATH_PACKED)
 driver = webdriver.Chrome(service=service, options=options)
 
-# TODO: add list with all options to use
-# with the control_bot_flow func
-
 
 def control_bot_flow() -> None:
+    read_settings_on_bot_load(ABSOLUTE_SETTINGS_PATH)
     print("Please sign in...")
     print(
         "After signing in, choose one of the following options:"
@@ -36,6 +41,8 @@ def control_bot_flow() -> None:
     print(f"You selected option: {user_input}")
     if int(user_input) == 1:
         scrape(driver, BUFF_MARKET_URL)
+    if int(user_input) == 2:
+        handle_settings()
     else:
         print("Please select a valid option.")
 
@@ -43,7 +50,6 @@ def control_bot_flow() -> None:
 def main():
     read_flags()
     driver.get(BUFF_BASE_URL)
-    driver.get(BUFF_MARKET_URL)
     control_bot_flow()
     log()
 
