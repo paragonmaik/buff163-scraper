188c39a019a188fbae84d4af54b65e11d062b643
maik <estevan.maik@gmail.com>
2024-02-02 09:44:47 -0300

    feat: adds writer module

---
+++ src/writer/writer.py
@@ -0,0 +1,40 @@
+"""
+    Skins list writing module.
+"""
+
+from skin import SKINS_LIST
+import json
+
+BASE_FILE_PATH = "scrapedfiles/"
+base_dict = list()
+
+
+def fill_base_dict() -> None:
+    for skin in SKINS_LIST:
+        skin_dict = f"""
+        "skinName": "{skin.name}",
+        "askQty": "{skin.ask_quantity}",
+        "askPrice": "{skin.ask_price}",
+        "bidQty": "{skin.bid_price}",
+        "bidPrice": "{skin.bid_price}",
+        "priceDiff": "{skin.price_diff}",
+        "pctDiff": "{skin.pct_diff}",
+        "wear": "{skin.wear}",
+        "url": "{skin.url}",
+        """
+        base_dict.append("{" + skin_dict + "}")
+
+
+def write(file_name: str) -> None:
+    if len(SKINS_LIST) > 0:
+        fill_base_dict()
+        with open(
+            BASE_FILE_PATH + file_name + ".json", "w"
+        ) as f:
+            json.dump(base_dict, f, indent=2)
+    else:
+        print(
+            """
+        List is empty, select the correct option to scrape for skins first.
+            """
+        )
